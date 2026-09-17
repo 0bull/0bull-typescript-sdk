9f38a2d473c2f3914cad19141fa9338229bfa0ae
0bull <github@0bull.net>
2026-09-17 09:41:26 +0000

    feat(session): create phone-controller sessions

--- src/resources/session.ts
+++ src/resources/session.ts
@@ -1,3 +1,16 @@
+import { asObject } from "../core.js";
+import type { ControllerSession } from "../types.js";
 import { Resource } from "./resource.js";
 
-export class SessionResource extends Resource {}
+export class SessionResource extends Resource {
+  /**
+   * Create a phone-controller session: the phones you can use and a short-lived `socket_url`.
+   * REST only. Most callers want `client.socket()` instead.
+   */
+  create(): Promise<ControllerSession> {
+    return this.transport.execute({
+      rest: { method: "GET", path: "/v1/phone-controller" },
+      parseRest: (response) => asObject<ControllerSession>(response.json()),
+    });
+  }
+}
